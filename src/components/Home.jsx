506ada5be3f4faa1e8869e9d2dd1af9b9c15963b
Johann4904 <johdokdci@web.de>
2024-01-20 18:15:14 +0100

    Change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <nav className='nav'>
         <ul>
           <li><Link to="/home">HOME</Link></li>
-          <li><Link to="/">FLANTZESUCHE</Link></li>
+          <li><Link to="/">FLANTZESUCH</Link></li>
           <li><Link to="/guide">GUIDE</Link></li>
           <li><Link to="/calender">CALENDER</Link></li>
         </ul>
