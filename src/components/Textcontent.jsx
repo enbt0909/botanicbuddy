2fadd04d0391250f8258122a35697bfaa1a305da
enbt0909 <eugenbeirit22@gmail.com>
2024-01-21 13:53:24 +0100

    new

---
+++ src/components/Textcontent.jsx
@@ -0,0 +1,15 @@
+
+
+const Textcontent = () => {
+    return (
+        <div className='text-box'>
+       <p>🌿 Willkommen bei BotanikBuddy! Dein ultimativer Pflanzenpflege-Begleiter🌿<br /><br />
+
+         🌿 BotanikBuddy ist eine App, die dir hilft, deine Pflanzen zu pflegen. Du kannst deine Pflanzen in der App speichern und erhältst Erinnerungen, wann du sie gießen musst. Außerdem kannst du deine Pflanzen mit anderen Pflanzeneltern teilen und dich mit ihnen austauschen.🌿<br /><br /><br /><br />
+         🌿"Vergisst du auch immer, wann deine Zimmerpflanzen Wasser brauchen? 🌿 <br />Suchst du nach Expertenrat, um deine Fähigkeiten als Pflanzeneltern zu verbessern? Hier ist die Lösung – BotanikBuddy, deine perfekte App!"
+       </p>
+     </div>
+    )
+}
+ 
+export default Textcontent;
